9a2cf60f44ef84b15dee415d505a998e5757165c
claire <clairebontempo@gmail.com>
2021-01-28 10:57:48 -0800

    commit tree 92901eed15fa312d52b62658e096f812176709e7

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.querySelector('main#main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+let content = "YOUR-NAME is the champion"
+newHeader.innerHTML = content
